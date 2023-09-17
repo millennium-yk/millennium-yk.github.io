1c7a69117a0429771eac1f52acb9a0b47599518a
유우카(优香) <142193893+yuuka66@users.noreply.github.com>
2023-09-17 14:34:47 +0000

    site update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='w-window h-window center-box'>
+        <span className='w-full center-box'>
+          <span className='logo'>Yuka</span>
+        </span>
+        <span className='btn center-box'>
+          <i className="fa-solid fa-play"></i>
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
