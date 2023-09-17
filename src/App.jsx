c5df112ce334aee500ceeeeb1d806c31d26f513f
유우카(优香) <142193893+yuuka66@users.noreply.github.com>
2023-09-17 15:29:01 +0000

    context false

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.oncontextmenu = function () { return false; }
+  })
   return (
     <>
       <div className='w-window h-window center-box white-layer'>
@@ -11,7 +15,7 @@ function App() {
           <i className="fa-solid fa-play"></i>
         </span>
       </div>
-      <video src="/train.mp4" type='video/mp4' controlsList="nodownload" onContextMenu={() => { return false; }} muted autoPlay loop className='movie'>
+      <video src="/train.mp4" type='video/mp4' controlsList="nodownload" muted autoPlay loop className='movie'>
       </video>
     </>
   );
