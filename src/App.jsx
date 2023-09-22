15833851e6b000f92b7572285f8c38f0307f4f3f
유우카(优香) <142193893+yuuka66@users.noreply.github.com>
2023-09-22 09:23:23 +0000

    to simply

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ function App() {
     document.oncontextmenu = function () { return false; }
   })
   return (
-    <>
-      <div className='w-window h-window center-box white-layer'>
-        <span className='w-full center-box'>
-          <span className='logo'>Yuka</span>
-        </span>
-        <span className='btn center-box'>
-          <i className="fa-solid fa-play"></i>
-        </span>
+    <main>
+      <div className='img_box'>
+        <img src="https://avatars.githubusercontent.com/u/142193893?v=4" alt='user_icon' />
+        <span className='img_title'>Yuka</span>
       </div>
-      <video src="/train.mp4" type='video/mp4' controlsList="nodownload" muted autoPlay loop className='movie'>
-      </video>
-    </>
+      <div className='text'>
+        Japanese🎌 vocational school student,<br />
+        Majoring in information engineering.💻 <br />
+        <b>Hobbies</b><i className="fa-solid fa-face-smile"></i><br />
+        <i className='fa-solid fa-gamepad'></i>Playing Game<br />
+        <i className='fa-solid fa-futbol'></i>Watching Football<br />
+        <b>Codes</b><i className="fa-solid fa-code"></i><br />
+        <i className="fa-brands fa-square-js"></i>JavaScript <br />
+        <i className="fa-brands fa-python"></i>Python <br />
+        <b>Contact</b><i className="fa-solid fa-comment"></i><br />
+        <i className="fa-brands fa-discord"></i>Discord yuka66 DM me!<br />
+        <i className="fa-brands fa-twitter"></i><a href="https://twitter.com/yuka_agar">Twitter</a><br />
+        <i className="fa-brands fa-facebook"></i><a href="https://www.facebook.com/profile.php?id=100069525129725">Facebook</a><br />
+      </div>
+      <span className='star'>🌟</span>
+    </main>
   );
 }
 
